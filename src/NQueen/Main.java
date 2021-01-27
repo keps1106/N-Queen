e7b5f43c8f01039238c0575b1fcfa82ab92f4aaf
毛絜妤 <clara.mao@tpisoftware.com>
2021-01-27 14:39:47 +0800

    no message

--- src/NQueen/Main.java
+++ src/NQueen/Main.java
@@ -1,60 +1,8 @@
+package NQueen;
+
 public class Main {
     public static void main(String[] args) {
         int n = 8;
-        nQueens(n);
-    }
-
-    private static void nQueens(int noOfQueens) {
-        int [] board = new int[noOfQueens];
-        placeQueen(board, 0, noOfQueens);
-    }
-
-    private static void placeQueen(int[] board, int current, int noOfQueens) {
-        if (current == noOfQueens) {
-            displayQueens(board);
-            return;
-        }
-
-        for (int i = 0; i < noOfQueens; i++) {
-            board[current] = i;
-            if (noKill(board, current)) {
-                placeQueen(board, current + 1, noOfQueens);
-            }
-        }
-    }
-
-    private static boolean noKill(int[] board, int currentColumnOfQueen) {
-
-        for (int i = 0; i < currentColumnOfQueen; i++) {
-            // same row
-            if (board[i] == board[currentColumnOfQueen])
-                return false;
-
-            // Diagonal
-            if ((currentColumnOfQueen - i) == Math.abs(board[currentColumnOfQueen] - board[i])) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static void displayQueens(int[] board) {
-        for (int value : board)
-            System.out.println(value +"\t");
-
-        System.out.print("\n");
-        int n = board.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int value : board) {
-                if (value == i)
-                    System.out.print("Q\t");
-                else
-                    System.out.print("*\t");
-            }
-            System.out.print("\n");
-        }
-
-        System.out.print("\n");
+        Service.nQueens(n);
     }
 }
--- src/NQueen/Service.java
+++ src/NQueen/Service.java
@@ -1,2 +1,57 @@
-package NQueen;public class Service {
+package NQueen;
+
+public class Service {
+
+    public static void nQueens(int noOfQueens) {
+        int [] board = new int[noOfQueens];
+        placeQueen(board, 0, noOfQueens);
+    }
+
+    private static void placeQueen(int[] board, int current, int noOfQueens) {
+        if (current == noOfQueens) {
+            displayQueens(board);
+            return;
+        }
+
+        for (int i = 0; i < noOfQueens; i++) {
+            board[current] = i;
+            if (noKill(board, current)) {
+                placeQueen(board, current + 1, noOfQueens);
+            }
+        }
+    }
+
+    private static boolean noKill(int[] board, int currentColumnOfQueen) {
+
+        for (int i = 0; i < currentColumnOfQueen; i++) {
+            // same row
+            if (board[i] == board[currentColumnOfQueen])
+                return false;
+
+            // Diagonal
+            if ((currentColumnOfQueen - i) == Math.abs(board[currentColumnOfQueen] - board[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void displayQueens(int[] board) {
+        for (int value : board) {
+            System.out.print(value + "\t");
+        }
+
+        int n = board.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int value : board) {
+                if (value == i)
+                    System.out.print("Q\t");
+                else
+                    System.out.print("*\t");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
 }
